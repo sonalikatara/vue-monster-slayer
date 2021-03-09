853d853b9f3e0e20162207fa84847cba8da1e189
Sonali Shukla <sonalikatara@gmail.com>
2021-03-08 19:52:45 -0800

    feat: add special attack

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data(){
         return {
             monsterHealth: 100,
-            playerHealth: 100
+            playerHealth: 100,
+            currentRound: 0,
         }
     },
     computed:{
@@ -15,19 +16,32 @@ const app = Vue.createApp({
         },
         playerHealthBarStyle(){
             return {width: this.playerHealth +'%'}
+        },
+        canSpecialAttack(){
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster(){
+           this.currentRound++;
            // calculate the  damage and reduce monster health by it
            // generate a random number between 5 and 12
            let damage =  getRandomValue(5,12);
            this.monsterHealth -= damage;
            this.attackPlayer();
+          
         },
+
         attackPlayer(){
             let damage =  getRandomValue(8,15);
             this.playerHealth -= damage;
+        },
+        
+        specialAttackMonster(){
+            this.currentRound++;
+            let damage =  getRandomValue(10,25);
+            this.monsterHealth -= damage;
+            this.attackPlayer();
         }
     },
 
